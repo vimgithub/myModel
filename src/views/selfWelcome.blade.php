3f638dc38e8a50f0061ba9fcd504b4caed9bea83
Zhaiwei <zhaiweiwei@easy2study.net>
2017-09-15 15:20:41 +0800

    修改测试数据

--- src/views/selfWelcome.blade.php
+++ src/views/selfWelcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{ $msg }}{{ $data }}
+                    {{ $msg }}
                 </div>
 
                 <div class="links">
